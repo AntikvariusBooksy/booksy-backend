2c7846f5ab360c855d2ef7001c3b3f4990409fde
AntikvariusBooksy <bookmansrl@gmail.com>
2026-04-24 00:17:09 +0200

    trigger: forced redeploy

--- main.py
+++ main.py
@@ -589,4 +589,4 @@ def test_cascade(bt: BackgroundTasks):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080) 
